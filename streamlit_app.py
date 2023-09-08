529b09416e44c7f64cfd92527e590e49087e286a
closedtuesdays <cr1319@gmail.com>
2023-09-07 21:10:25 -0500

    Update streamlit_app.py
    testing correction on line 72, Snowflake insert command

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,7 +69,7 @@ if streamlit.button('Get Fruit List'):
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 def insert_row_snowflke(new_fruit):
   with my_cnx.cursor() as my_cur:
-    my.cur.execute("insert into fruit_load_list values ('" + ???? +"')")
+    my_cur.execute("insert into fruit_load_list values ('" + add_my_fruit +"')")
     return "Thanks for adding "  new_fruit
 
                 
